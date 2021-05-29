9e221f3f197e5d6c0b6b64fbf175eaf340318d81
UoA-sjer215 <68827415+UoA-sjer215@users.noreply.github.com>
2021-05-29 14:43:32 +1200

    updated book dataprovider
    following the tutorial instructions given, not completed

--- Design1/app/src/main/java/com/example/design1/MainActivity.java
+++ Design1/app/src/main/java/com/example/design1/MainActivity.java
@@ -11,50 +11,86 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+class Books {
+    String name;
+    String author;
+    int viewed;
+    char genre;
+    String id;
+    int pic;
+
+    public Books (String name, String author, int viewed, char genre, String id, int pic){
+        this.name = name;
+        this.author = author;
+        this.viewed = viewed;
+        this.genre = genre;
+        this.pic = pic;
+        this.id = id;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
+
+
+
+class BookProvider{
+    static String[] titles = {"Space Marines", "Raven Guard", "Fundamentals of Thermal-Fluid Sciences",
+            "Mechanical Engineering Design", "Electrical Engineering", "Chemicals and Materials",
+            "Rocket Propulsion Elements","Essential Mathematics for Engineers","Bridge Building 101", "Biomedical",
+            "Heart", "Lungs", "Liver", "Stomach", "Brain","Kidney (bean)", "Pancreas", "Gall Bladder","Intestines",
+            "Anal Canal","Robbery", "Chose and Action", "Case Closed", "Fiduciary Obligations", "Breaches",
+            "Conflict of Interests", "Order in the Court", "Agency","Negligent Misstatement ","the Doctrine of Indefeasibility"};
+
+    static String[] ids = {"001", "002", "003", "004", "005", "006", "007",
+            "008", "009", "010", "011", "012", "013", "014", "015", "016", "017",
+            "018", "019", "020","021", "022", "023", "024", "025", "026", "027",
+            "028", "029", "030"};
+
+    static String[] authors = {"Jane Austen", "William Shakespeare",
+            "William Shakespeare", "William Shakespeare", "William Shakespeare", "Arthur Golden",
+            "Jane Austen", "Tony Hawk", "William Shakespeare", "Walter Dean Myers","Jane Austen", "William Shakespeare",
+            "William Shakespeare", "William Shakespeare", "William Shakespeare", "Arthur Golden",
+            "Jane Austen", "Tony Hawk", "William Shakespeare", "Walter Dean Myers","Jane Austen", "William Shakespeare",
+            "William Shakespeare", "William Shakespeare", "William Shakespeare", "Arthur Golden",
+            "Jane Austen", "Tony Hawk", "William Shakespeare", "Walter Dean Myers"};
+
+    //image path of book covers in the drawable folder
+    static int[] coveraddrs = {R.drawable.b100};
+    static ArrayList<Books> eng  = new ArrayList<>();
+    static ArrayList<Books> law  = new ArrayList<>();
+    static ArrayList<Books> med  = new ArrayList<>();
+    static ArrayList<Books> search  = new ArrayList<>();
+
+    public void  generateData() {
+        for (int i = 0; i < 30; i++) {
+            String id = ids[i];
+            String author = authors[i];
+            String title = titles[i];
+            int coveraddr = coveraddrs[0];
+            int views = 5;
+            if (i<10){
+                Books aBook = new Books(title,author,views,'e',id,coveraddr);
+                eng.add(aBook);
+            }else if (i<20){
+                Books aBook = new Books(title,author,views,'m',id,coveraddr);
+                med.add(aBook);
+            }else{
+                Books aBook = new Books(title,author,views,'l',id,coveraddr);
+                law.add(aBook);
+            }
+
+
+        }
+    }
+}
+
 public class MainActivity extends AppCompatActivity {
     class ViewHolder{
 
     }
 
-    class Books {
-        String name;
-        String author;
-        int viewed;
-        char genre;
-        int id;
-        int isbn;
-        String pic;
-        String blurb;
 
-        public Books (String name, String author, int viewed, char genre,int id, int isbn, String pic, String blurb){
-            this.name = name;
-            this.author = author;
-            this.viewed = viewed;
-            this.genre = genre;
-            this.isbn = isbn;
-            this.pic = pic;
-            this.blurb = blurb;
-            this.id = id;
-            if (genre == 'e'){
-                eng.add(this);
-            }
-            else if (genre == 'l'){
-                law.add(this);
-            }
-            else{
-                med.add(this);
-            }
-        }
-        public String getName(){
-            return this.name;
-        }
-
-
-    }
-    ArrayList<Books> eng  = new ArrayList<>();
-    ArrayList<Books> law  = new ArrayList<>();
-    ArrayList<Books> med  = new ArrayList<>();
-    ArrayList<Books> search  = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,25 +104,35 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onFirst(View view){
         Intent optionFirst = new Intent(this, OptionFirst.class);
-        optionFirst.putExtra("toDisplay", eng);
+        optionFirst.putExtra("toDisplay", BookProvider.eng);
         startActivity(optionFirst);
     }
     public void onSecond(View view){
         Intent optionFirst = new Intent(this, OptionFirst.class);
-        optionFirst.putExtra("toDisplay", law);
+        optionFirst.putExtra("toDisplay", BookProvider.law);
         startActivity(optionFirst);
     }
     public void onThird(View view){
         Intent optionFirst = new Intent(this, OptionFirst.class);
-        optionFirst.putExtra("toDisplay", med);
+        optionFirst.putExtra("toDisplay", BookProvider.med);
         startActivity(optionFirst);
     }
 
     public void searchBook (String searchTerm){
-        search.clear();
-        for (Books book: eng){
+        BookProvider.search.clear();
+        for (Books book: BookProvider.eng){
             if (nameMatch(book.getName(),searchTerm)){
-                search.add(book);
+                BookProvider.search.add(book);
+            }
+        }
+        for (Books book: BookProvider.med){
+            if (nameMatch(book.getName(),searchTerm)){
+                BookProvider.search.add(book);
+            }
+        }
+        for (Books book: BookProvider.law){
+            if (nameMatch(book.getName(),searchTerm)){
+                BookProvider.search.add(book);
             }
         }
     }
--- Design1/app/src/main/java/com/example/design1/OptionFirst.java
+++ Design1/app/src/main/java/com/example/design1/OptionFirst.java
@@ -13,7 +13,58 @@ import java.util.List;
 public class OptionFirst extends AppCompatActivity {
 
     String displayType;
-    ArrayList<MainActivity.Books> booksToDisplay  = new ArrayList<>();
+    ArrayList<Books> booksToDisplay  = new ArrayList<>();
+
+
+    public class BookAdapter extends ArrayAdapter<Books> {
+        private static class ViewHolder{
+            public ImageView ivCover;
+            public TextView tvTitle;
+            public TextView tvAuthor;
+        }
+        public BookAdapter(Context context, ArrayList<Book> aBooks){
+            super(context, 0, aBooks); }
+        //translates a particular 'Book' given a position
+        //into a relevant row within an AdapterView
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            //get the data item for this position
+            final Book book = getItem(position);
+            //check if an existing view is being reused, otherwise inflate the
+            view
+            ViewHolder viewHolder;
+            if (convertView == null){
+                viewHolder = new ViewHolder();
+                LayoutInflater inflater =
+                        (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVIC
+                                E);
+                convertView = inflater.inflate(R.layout.item_book,parent,false);
+                viewHolder.ivCover =
+                        (ImageView)convertView.findViewById(R.id.ivBookCover);
+                viewHolder.tvTitle =
+                        (TextView)convertView.findViewById(R.id.tvTitle);
+                viewHolder.tvAuthor =
+                        (TextView)convertView.findViewById(R.id.tvAuthor);
+                convertView.setTag(viewHolder);
+            } else{
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            //Populate the data into the template view using the data object
+            viewHolder.tvTitle.setText(book.getTitle());
+            viewHolder.tvAuthor.setText(book.getAuthor());
+            int resID = book.getCover();
+            viewHolder.ivCover.setImageResource(resID);
+            return convertView;
+        }
+    }
+
+
+
+
+
+
+
+
 
 
 
@@ -22,7 +73,7 @@ public class OptionFirst extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_first);
         Intent thisIntent = getIntent();
-        booksToDisplay = (ArrayList<MainActivity.Books>) thisIntent.getSerializableExtra("list");
+        booksToDisplay = (ArrayList<Books>) thisIntent.getSerializableExtra("list");
         https://stackoverflow.com/questions/8109867/array-list-of-objects-via-intent
 
         if (displayType.equals('e')){
