8318f86b1f2b0a1fc3de25380878723778de254c
UoA-sjer215 <68827415+UoA-sjer215@users.noreply.github.com>
2021-05-31 15:16:09 +1200

    Implemented basic animation for switching activity. Adjusted text size to display correctly. Begun implementing pageviewer for detailed book view.

--- Design1/app/src/main/java/com/example/design1/MainActivity.java
+++ Design1/app/src/main/java/com/example/design1/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.widget.SearchView;
 import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.transition.Slide;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -23,12 +26,16 @@ import java.util.Scanner;
 public class MainActivity extends AppCompatActivity {
 
     public static final String BOOKS_DETAIL_KEY = "books";
+    Window thisWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        thisWindow = getWindow();
+        getWindow().requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS);
+        getWindow().setExitTransition(new Slide());
+        getWindow().setEnterTransition(new Slide());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void onCart(View view){
@@ -37,14 +44,14 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onFirst(View view){
         Intent optionFirst = new Intent(this, OptionFirst.class);
-        startActivity(optionFirst);
+        startActivity(optionFirst, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
     public void onSecond(View view){
         Intent optionFirst = new Intent(this, OptionSecond.class);
-        startActivity(optionFirst);
+        startActivity(optionFirst, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
     public void onThird(View view){
         Intent optionFirst = new Intent(this, OptionThird.class);
-        startActivity(optionFirst);
+        startActivity(optionFirst, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }
